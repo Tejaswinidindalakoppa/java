25158adb5c6244e1b69cdc1ba617b61748595e25
Tejaswini Dindalakoppa <tejcoding004@gmail.com>
2024-09-25 22:25:19 +0530

    Add files via upload

---
+++ Main22.java
@@ -0,0 +1,8 @@
+public class Main22 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+        System.out.println(x >= y); // returns true because 5 is greater, or equal, to 3
+    }
+}
+  
