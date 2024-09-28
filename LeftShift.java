d7f1d839e2b4f182087abd6007eb435a710f11a0
Tejaswini Dindalakoppa <tejcoding004@gmail.com>
2024-09-28 19:18:00 +0530

    Add files via upload

---
+++ LeftShift.java
@@ -0,0 +1,9 @@
+public class LeftShift {
+    public static void main(String[] args) {
+        System.out.println(5 << 1);  // 0101 << 1 = 1010 (10)
+        System.out.println(3 << 2);  // 0011 << 2 = 1100 (12)
+        System.out.println(7 << 3);  // 0111 << 3 = 111000 (56)
+        System.out.println(1 << 4);  // 0001 << 4 = 10000 (16)
+        System.out.println(2 << 1);  // 0010 << 1 = 0100 (4)
+    }
+}
