c8aae8eb2bf2f4a1efab600e702d1664e94ec882
Tejaswini Dindalakoppa <tejcoding004@gmail.com>
2024-10-24 21:08:44 +0530

    Add files via upload

---
+++ MatrixMulti.java
@@ -0,0 +1,29 @@
+public class MatrixMulti {
+    public static void multiplyMatrices(int[][] a, int[][] b) {
+        int rowsA = a.length;
+        int colsA = a[0].length;
+        int colsB = b[0].length;
+        
+        int[][] result = new int[rowsA][colsB];
+        
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                for (int k = 0; k < colsA; k++) {
+                    result[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        
+        for (int[] row : result) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int[][] a = {{1, 2}, {3, 4}};
+        int[][] b = {{5, 6}, {7, 8}};
+        multiplyMatrices(a, b); // Output: 19 22 43 50
+    }
+}
---
+++ Mergingarrays.java
@@ -0,0 +1,29 @@
+public class Mergingarrays {
+    public static int[] mergeSortedArrays(int[] arr1, int[] arr2) {
+        int[] result = new int[arr1.length + arr2.length];
+        int i = 0, j = 0, k = 0;
+        
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                result[k++] = arr1[i++];
+            } else {
+                result[k++] = arr2[j++];
+            }
+        }
+        while (i < arr1.length) {
+            result[k++] = arr1[i++];
+        }
+        while (j < arr2.length) {
+            result[k++] = arr2[j++];
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 5};
+        int[] arr2 = {2, 4, 6};
+        int[] merged = mergeSortedArrays(arr1, arr2);
+        for (int i : merged) {
+            System.out.print(i + " "); // Output: 1 2 3 4 5 6
+        }
+    }
+}
