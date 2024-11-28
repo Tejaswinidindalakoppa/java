83b21c9f7fb3e7660790b031e22ad3cd4196254f
Tejaswini Dindalakoppa <tejcoding004@gmail.com>
2024-11-28 23:00:58 +0530

    Add files via upload

---
+++ SumCalculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SumCalculator {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter the first number: ");
+            int num1 = scanner.nextInt();
+            
+            System.out.print("Enter the second number: ");
+            int num2 = scanner.nextInt();
+            
+            int sum = num1 + num2;
+            
+            System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        }
+    }
+}
